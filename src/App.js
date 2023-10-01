91d7602c7ac49828a16bc501a3a3343c63d263a4
Milene Desjardins <m.desjardins31@outlook.com>
2023-10-01 16:22:43 -0400

    Create React App files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        OrthoBien-Etre
+      </header>
+    </div>
+  );
+}
+
+export default App;
