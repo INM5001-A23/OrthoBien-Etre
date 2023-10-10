cbb783c8f43bd94741ec8986046f50a59289ff2c
Milene Desjardins <m.desjardins31@outlook.com>
2023-10-10 16:44:16 -0400

    gitignore dans backend

--- frontend/src/components/Bouton.js
+++ frontend/src/components/Bouton.js
@@ -2,11 +2,9 @@ import Button from "react-bootstrap/Button";
 
 function Bouton({ variant, nom }) {
   return (
-    <>
-      <Button type="submit" variant={variant}>
-        {nom}
-      </Button>{" "}
-    </>
+    <Button type="submit" variant={variant}>
+      {nom}
+    </Button>
   );
 }
 
