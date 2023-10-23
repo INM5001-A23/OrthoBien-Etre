1f45f523f49c957158b17cc206da1b7cf9212bed
Milene Desjardins <m.desjardins31@outlook.com>
2023-10-23 12:39:33 -0400

    Affichage nom des categories dans carte produit

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,35 +1,46 @@
-import express from 'express';
-import Produits from '../models/Produits.js';
+import express from "express";
+import Produits from "../models/Produits.js";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const produit = await Produits.find()
-        res.json(produit)
-    } catch (err){
-    res.status(500).json({message: err.message})
-    }
+router.get("/", async (req, res) => {
+  try {
+    const produit = await Produits.find();
+    res.json(produit);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Fetch popular products
-router.get('/popular', async (req, res) => {
-    try{
-        const popularProduct = await Produits.find({popularity: 100}) 
-        res.json(popularProduct)
-    } catch (err){
-        res.status(500).json({message: err.message})
-    }
+router.get("/popular", async (req, res) => {
+  try {
+    const popularProduct = await Produits.find({ popularity: 100 });
+    res.json(popularProduct);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Fetch produits phares
-router.get('/produitPhare', async (req, res) => {
-    try{
-        const produitPhare = await Produits.find({produitPhare: 1})
-        res.json(produitPhare)
-    } catch (err){
-        res.status(500).json({message: err.message})
-    }
+router.get("/produitPhare", async (req, res) => {
+  try {
+    const produitPhare = await Produits.find({ produitPhare: 1 });
+    res.json(produitPhare);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Fetch all categories
+router.get("/:code", async (req, res) => {
+  try {
+    const codeCategorie = req.params.code;
+    const categorie = await Categories.findOne({ codeCategorie });
+    res.json(categorie);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 export default router;
