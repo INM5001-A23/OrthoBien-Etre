00050c60c7ce3f01d2defd8c9abcddfdda671573
Nawal Tatari <tatari.nawal@courrier.uqam.ca>
2023-11-02 17:46:59 -0400

    Ajout modification pages  panier et paiement

--- frontend/src/pages/Paiement.js
+++ frontend/src/pages/Paiement.js
@@ -1,7 +1,7 @@
-import { Container, Stack } from "react-bootstrap";
-import ModelePage from "../layout/ModelePage";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AxiosContext } from "..";
+import ModelePage from "../layout/ModelePage";
+import { Container, Stack, Form, Button } from "react-bootstrap";
 
 function PagePaiement() {
   const axios = useContext(AxiosContext);
@@ -26,7 +26,32 @@ function PagePaiement() {
 
   return (
     <ModelePage>
-      <Stack gap={3}></Stack>
+      <Container>
+        <Stack gap={3}>
+          <h1>Page de paiement</h1>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Nom sur la carte</Form.Label>
+              <Form.Control type="text" placeholder="Entrez le nom sur la carte" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Numéro de carte de crédit</Form.Label>
+              <Form.Control type="text" placeholder="Entrez le numéro de carte de crédit" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Date d'expiration</Form.Label>
+              <Form.Control type="text" placeholder="MM/AA" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Code de sécurité</Form.Label>
+              <Form.Control type="text" placeholder="CVC" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Payer
+            </Button>
+          </Form>
+        </Stack>
+      </Container>
     </ModelePage>
   );
 }
