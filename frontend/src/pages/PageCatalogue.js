05d73d92b3b466e7b6c7087d1963b8462eea4dfd
Milene Desjardins <m.desjardins31@outlook.com>
2023-11-08 19:59:09 -0500

    correction syntaxes

--- frontend/src/pages/PageCatalogue.js
+++ frontend/src/pages/PageCatalogue.js
@@ -13,11 +13,7 @@ function PageCatalogue() {
 
   useEffect(() => {
     axios
-<<<<<<< HEAD:frontend/src/pages/Catalogue.js
       .get("/produits")
-=======
-      .get("/Produits")
->>>>>>> 90ffdf5a01267268243ec49abd5454a7afb06fb1:frontend/src/pages/PageCatalogue.js
       .then(function (response) {
         // handle success
         console.log(response);
@@ -28,6 +24,8 @@ function PageCatalogue() {
         console.log(error);
       })
       .finally(function () {
+        // always executed
+      });
   }, [axios]);
 
   return (
