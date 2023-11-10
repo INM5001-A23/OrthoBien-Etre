70497204f6d1b20c28e1aaa60b980e9199289e47
danyclaraexume <dany.exume@outlook.com>
2023-11-10 15:18:52 -0500

    Simplify code for payment page - error fixed

--- frontend/src/pages/Paiement.js
+++ frontend/src/pages/Paiement.js
@@ -2,28 +2,28 @@ import React, { useContext, useEffect, useState } from "react";
 import { AxiosContext } from "..";
 import ModelePage from "../layout/ModelePage";
 import { Container, Stack, Form, Button } from "react-bootstrap";
-import StripePayment from "./StripePayment";
+import StripePayment from "../components/StripePayment";
 
 function PagePaiement() {
-  const axios = useContext(AxiosContext);
-  const [items, setItems] = useState([]);
+  // const axios = useContext(AxiosContext);
+  // const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/categories")
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        setItems(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/categories")
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response);
+  //       setItems(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
+  //       // always executed
+  //     });
+  // }, []);
 
   return (
     <ModelePage>
