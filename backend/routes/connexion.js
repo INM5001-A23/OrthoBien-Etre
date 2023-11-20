29c0cd483b0b3038f6f9d8795185789b13db69ed
Milene Desjardins <m.desjardins31@outlook.com>
2023-11-19 20:21:38 -0500

    Ajout authentification avec JWT

--- backend/routes/connexion.js
+++ backend/routes/connexion.js
@@ -2,6 +2,7 @@ import express from "express";
 import Connexion from "../models/Connexion.js";
 import Clients from "../models/Clients.js";
 import Administrateurs from "../models/Administrateurs.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 router.use(express.json());
@@ -15,67 +16,92 @@ router.use(express.json());
  * @returns Et les values nécessaire au profil client ou profil admin
  */
 
-router.post('/', async (req, res) => {
-    try{
+router.post("/", async (req, res) => {
+  try {
+    //Information de connexion
 
-        //Information de connexion
+    const { courriel, mdp } = req.body;
 
-        const {courriel,mdp} =req.body;
+    //veification de l'acces admin
 
-        //veification de l'acces admin
+    const admin = await Administrateurs.findOne(
+      { courriel },
+      "courriel mdp prenomAdmin codeAdmin "
+    );
 
-        const admin = await Administrateurs.findOne({courriel}, 'courriel mdp prenomAdmin codeAdmin ');
+    if (!admin) {
+      //Verification de l'acces client
+      const user = await Clients.findOne(
+        { courriel },
+        "courriel mdp prenom nom rue ville province codePostal telephone"
+      );
 
-        if (!admin) {
+      if (!user) {
+        return res
+          .status(501)
+          .json({ erreur: "Votre courriel ou mot de passe est incorrecte" });
+      }
 
-            //Verification de l'acces client
-            const user = await Clients.findOne({courriel},'courriel mdp prenom nom rue ville province codePostal telephone');
-        
-            if (!user){
-                return res.status(501).json({erreur : "Votre courriel ou mot de passe est incorrecte"})
-            }
+      //return : message d'accueil + valeurs pour page profil
 
-            //return : message d'accueil + valeurs pour page profil
+      if (user.mdp == mdp) {
+        const auth = {
+          role: "client",
+          message: "Bonjour " + user.prenom,
+          prenom: user.prenom,
+          courriel: user.courriel,
+          prenom: user.prenom,
+          nom: user.nom,
+          rue: user.rue,
+          ville: user.ville,
+          province: user.province,
+          codePostal: user.codePostal,
+          telephone: user.telephone,
+        };
 
-            if(user.mdp == mdp){
-                console.log(user.mdp, user.courriel, user.prenom)
-                return res.status(201).json({success: true, message: "Bonjour " + user.prenom,
-                prenom : user.prenom,
-                courriel: user.courriel , 
-                prenom:user.prenom, 
-                nom : user.nom,
-                rue : user.rue, 
-                ville: user.ville ,
-                province : user.province, 
-                codePostal : user.codePostal, 
-                telephone: user.telephone
-            })
-            }else{
-                return res.status(501).json({erreur : "Votre courriel ou mot de passe est incorrecte"})
+        const token = jwt.sign(auth, process.env.JWT_SECRET);
 
-            }
-        }else{
+        return res.status(200).json({
+          success: true,
+          token,
+          ...auth,
+        });
+      } else {
+        return res
+          .status(401)
+          .json({ erreur: "Votre courriel ou mot de passe est incorrecte" });
+      }
+    } else {
+      if (admin.mdp == mdp) {
+        //return message d'accueil + value pour page profil admin au besoin
 
-            if(admin.mdp == mdp){
+        const auth = {
+          role: "admin",
+          message:
+            "Bonjour " +
+            admin.prenomAdmin +
+            " vous êtes connectés comme administrateur",
+          prenomAdmin: admin.prenomAdmin,
+          codeAdmin: admin.codeAdmin,
+        };
 
-                //return message d'accueil + value pour page profil admin au besoin
+        const token = jwt.sign(auth, process.env.JWT_SECRET);
 
-                return res.status(202).json({success: true, 
-                message: "Bonjour " + admin.prenomAdmin + " vous êtes connectés comme administrateur",
-                prenomAdmin : admin.prenomAdmin,
-                codeAdmin : admin.codeAdmin
-             })
-            }else{
-                return res.status(505).json({erreur : "Courriel admin ou mot de passe incorrect"})
-
-            }
-        }
-
-    }catch(error){
-        console.log("He is sure not connected" + error.message);
-        res.status(510).json({erreur: "definetly not connected at all lol"})
+        return res.status(200).json({
+          success: true,
+          token,
+          ...auth,
+        });
+      } else {
+        return res
+          .status(401)
+          .json({ erreur: "Courriel admin ou mot de passe incorrect" });
+      }
     }
-}
-)
+  } catch (error) {
+    console.log("He is sure not connected" + error.message);
+    res.status(510).json({ erreur: "definetly not connected at all lol" });
+  }
+});
 
 export default router;
