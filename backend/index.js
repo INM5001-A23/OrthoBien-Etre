9cc46d7d5e7c1fe93ce8672922b6eb86b5e280c7
danyclaraexume <dany.exume@outlook.com>
2023-12-10 20:27:13 -0500

    user cart update in mongo db - bug fixed

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,8 @@ const upload = multer();
 
 // enable cors
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(upload.array("files", 12));
 
