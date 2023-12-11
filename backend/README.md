4a993939e2048b5ec3dea8e5dd05b99b122a41fc
Daisy Exume <exume.daisy@courrier.uqam.ca>
2023-12-10 19:41:07 -0500

    modification page recherche

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,44 @@
 # OrthoBien-Etre-Backend
+
+## Installation
+
+1. Accédez au répertoire frontend du projet :
+
+```bash
+    cd .\backend\
+```
+
+2. Installer les dépendances
+
+```bash
+   npm install
+
+   npm i --save
+```
+
+3. Démarrer le serveur
+
+```bash
+    npm run start
+```
+
+4. Accédez à l'application dans votre navigateur : http://localhost:3300
+
+## Déploiement
+
+Pour déployer l'application en production, utilisez la commande:
+
+```bash
+    npm run build
+```
+
+## Configuration
+
+1. Copiez le fichier `.env.example` et renommez-le en `.env`.
+
+## Structure du Projet
+    `index.js`: Application backend
+    `/models` : Contient les modèles de la base de données.
+    `/routes` : Contient les routes qui font la connection backend et frontend.
+        
+
--- frontend/src/pages/PageRecherche.js
+++ frontend/src/pages/PageRecherche.js
@@ -2,12 +2,8 @@ import ModelePage from "../layout/ModelePage";
 import CarteProduit from "../components/CarteProduit";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { useContext, useEffect, useState } from "react";
-import { AxiosContext } from "..";
-import { Button, Container, Stack } from "react-bootstrap";
-import FiltreCatalogue from "../components/FiltreCatalogue";
-import { useSearchParams } from "react-router-dom";
-import FiltreCategorie from "../components/FiltreCategorie";
+import {  useEffect  } from "react";
+import {  Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 
 function Recherche  ({ }){
@@ -15,61 +11,26 @@ function Recherche  ({ }){
   const location = useLocation();
  
   const results = location.state.total;
-
-  const [ setResults] = useState([]);
-  const axios = useContext(AxiosContext);
-  const [searchParams] = useSearchParams();
-  const [filtreCategorie, setFiltreCategorie] = useState(
-    searchParams.get("filtreCategorie") || ""
-  );
   
-  const [filtre, setFiltre] = useState("");
 
   useEffect(() => {
     
-   console.log(results)
+
 
     if (results && results.length > 0) {
       
       return;
     }
-  
-    
-    const fetchData = async () => {
-      try {
-        // Update the API endpoint based on your server setup
-        const response = await fetch(`/api/search?filtre=${filtre}&categorie=${filtreCategorie}`);
-        const data = await response.json();
-        setResults(data);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
-    };
-    
-    
 
-}, [axios, filtre, filtreCategorie, results]);
+}, [ results]);
 
 
   return (
     <ModelePage>
       <Container>
-        <Stack direction="horizontal" gap={2}>
-          <FiltreCatalogue filtre={filtre} setFiltre={setFiltre} />
-          <FiltreCategorie
-            filtre={filtreCategorie}
-            setFiltre={setFiltreCategorie}
-          />
-          <Button
-            style={{ margin: "0px 0px 15px 0px" }}
-            onClick={() => {
-              setFiltre(""); // Réinitialise le filtre principal
-              setFiltreCategorie(""); // Réinitialise le filtre de catégorie
-            }}
-          >
-            Réinitialiser
-          </Button>
-        </Stack>
+        <h2 className="p-3">
+          Résultat de la recherche
+        </h2>
         <Row xs={1} md={4} className="g-4 justify-content-center">
           {results && results
             .map((produit) => (
